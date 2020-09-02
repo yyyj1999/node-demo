79c31a574d8722df17693a3ed98344643064f623
yuanjian <yuanjian@aiyunxiao.com>
2020-09-02 18:07:42 +0800

    mod: 调整eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true,
-	},
-	'extends': 'eslint:recommended',
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
+    'env': {
+        'browser': true,
+		'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
 		],
 		'spaced-comment': [// 注释后需要一个空格
 			'error',
@@ -38,6 +39,12 @@ module.exports = {
 			'error',
 			{ max: 1 },
 		],
-		'no-extra-semi': 'error',
-	}
+        'no-extra-semi': 'error',
+        "linebreak-style": [0, "error", "windows"],
+        "comma-spacing": ["error", { "before": false, "after": true }], // 逗号后强制空格
+        "keyword-spacing": ["error", { "before": true, "after": true }], // 关键字前后强制空格
+        "dot-notation": "error",    // 强制要求使用点号
+        "space-infix-ops": "error", // 强制操作符前后有空格
+        "key-spacing": ["error", { "afterColon": true }], // 冒号后空格
+    },
 };
